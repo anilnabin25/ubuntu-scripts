c015dc5d51e73957364098a4946379a3c026b293
Divya Shrestha <divyashrestha042@gmail.com>
2021-03-26 16:40:09 +0545

    script for git installation

---
+++ congifure-git.sh
@@ -0,0 +1,11 @@
+set -e
+echo "== Configured git =="
+git config --global color.ui true
+git config --global user.name "Divya Shrestha"
+git config --global user.email "divyashrestha042@gmail.com"
+
+echo "== Configured New Data =="
+git config --list
+
+# for mannaula configure please folow the below comand 
+# gedit ~/.gitconfig
